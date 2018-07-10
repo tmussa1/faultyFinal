d5fd0c4265b874718ed24430a2915d2b96716c4b
GBTC441016ur <fadhld92@gmail.com>
2018-07-10 11:37:29 -0400

    getters/setters

--- src/main/java/com/example/demo/models/Instructure.java
+++ src/main/java/com/example/demo/models/Instructure.java
@@ -1,4 +1,55 @@
 package com.example.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+
+
 public class Instructure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+    private String name;
+    private String department;
+    private String officeNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+
+
 }
--- src/main/java/com/example/demo/models/Student.java
+++ src/main/java/com/example/demo/models/Student.java
@@ -1,4 +1,50 @@
 package com.example.demo.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String major;
+    private String yearOfEntry;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getYearOfEntry() {
+        return yearOfEntry;
+    }
+
+    public void setYearOfEntry(String yearOfEntry) {
+        this.yearOfEntry = yearOfEntry;
+    }
+
+
 }
